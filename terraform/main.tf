4a95f02ac63fc7fce8b5c91b03100bf0de001bc3
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-15 12:37:15 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,8 @@
-provider "azurerm" {
-  features {}
-  version = "~> 4.56.0"
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacct"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_app_service" "example" {
-  name                = "example-appservice"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-  site_config {
-    dotnet_framework_version = "v4.0"
-  }
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_sql_server" "example" {
-  name                = "example-sqlserver"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  version             = "12.0"
-  administrator_login = "sqladmin"
-  administrator_login_password = var.sql_password
-}
-
-resource "azurerm_sql_database" "example" {
-  name                = "example-sqldatabase"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
-  requested_service_objective_name = "S0"
-  min_capacity = 0.5
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket-terraform"
+  acl    = "private"
 }
