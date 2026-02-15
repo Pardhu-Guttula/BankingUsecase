96be7b963328ccdae778457b666e689942c592b2
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 02:00:38 +0530

    commited by n8n

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+# Epic Title: Implement Adaptive Layouts
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/db_name'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+
+
+# File 2: Base HTML Template for Adaptive Layouts in authentication/templates/base.html
