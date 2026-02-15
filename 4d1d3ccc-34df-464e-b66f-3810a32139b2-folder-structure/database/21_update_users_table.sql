1c9931e2b6b58ab75ddf6680d2e096f40f326b2e
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 02:46:01 +0530

    Delete 4d1d3ccc-34df-464e-b66f-3810a32139b2-folder-structure directory

--- 4d1d3ccc-34df-464e-b66f-3810a32139b2-folder-structure/database/21_update_users_table.sql
+++
@@ -1,9 +0,0 @@
-# Epic Title: Role-based Access Control
-
-ALTER TABLE users
-ADD COLUMN role_id INT NOT NULL,
-ADD CONSTRAINT fk_role
-FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE;
-
-
-# File 9: requirements.txt Update
