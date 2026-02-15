f8fddea32639872b2baed349fea72e1bad12e053
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-15 13:27:48 +0530

    commited by n8n

---
+++ 71746896-9b68-4fdf-a1d7-4017ea01815e-folder-structure/backend/controllers/dashboard/adaptive_resolution_controller.py
@@ -0,0 +1,7 @@
+# Epic Title: Adaptive Screen Resolutions
+
+from django.shortcuts import render
+
+def adaptive_dashboard_view(request):
+    # Epic Title: Adaptive Screen Resolutions
+    return render(request, 'dashboard/adaptive_index.html')
