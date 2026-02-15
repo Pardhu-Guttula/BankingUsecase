f296036bf8d4bc21c20f5f23a71028158fc4bfa5
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-15 13:28:39 +0530

    commited by n8n

---
+++ 71746896-9b68-4fdf-a1d7-4017ea01815e-folder-structure/backend/services/dashboard/feature_service.py
@@ -0,0 +1,12 @@
+# Epic Title: Quick Access to Features
+
+class FeatureService:
+    @staticmethod
+    def get_essential_features():
+        # Epic Title: Quick Access to Features
+        return [
+            {'name': 'View Account', 'url': '/account/view'},
+            {'name': 'Transfer Funds', 'url': '/account/transfer'},
+            {'name': 'Pay Bills', 'url': '/account/payments'},
+            {'name': 'Investment Options', 'url': '/account/investments'},
+        ]
