f822d7060c75c2781e457823cf5ce806938a4005
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 03:51:48 +0530

    commited by n8n

--- backend/controllers/home_controller.py
+++ backend/controllers/home_controller.py
@@ -6,7 +6,6 @@ home_controller = Blueprint('home_controller', __name__)
 
 @home_controller.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-
-# File 4: Home Template in templates/home.html
+# File 5: Register Home Controller Blueprint in app.py (Already Exists, Modified)
