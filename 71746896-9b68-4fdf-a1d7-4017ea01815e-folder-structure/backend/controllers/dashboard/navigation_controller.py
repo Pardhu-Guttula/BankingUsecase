ae86e507eeb936d3fd08e34b6a5c8b4464354108
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-15 13:27:36 +0530

    commited by n8n

---
+++ 71746896-9b68-4fdf-a1d7-4017ea01815e-folder-structure/backend/controllers/dashboard/navigation_controller.py
@@ -0,0 +1,7 @@
+# Epic Title: Consistent User Experience
+
+from django.shortcuts import render
+
+def navigation_view(request):
+    # Epic Title: Consistent User Experience
+    return render(request, 'dashboard/navigation.html')
