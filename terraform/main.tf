9561079d377d77a772c5433f0d64eedec6280919
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-15 11:21:28 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,106 +1,127 @@
-provider {
-  azurerm = {
-    source  = "hashicorp/azurerm"
-    version = "~> 4.56.0"
-  }}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.56.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "self_service_rg" {
-  name     = "self-service-rg"
-  location = "East US"
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_app_service_plan" "service_plan" {
-  name                = "self-service-plan"
-  location            = azurerm_resource_group.self_service_rg.location
-  resource_group_name = azurerm_resource_group.self_service_rg.name
+resource "azurerm_app_service_plan" "asp" {
+  name                = "app-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku {
     tier = "Standard"
     size = "S1"
   }
 }
 
-resource "azurerm_app_service" "app_service" {
-  name                = "self-service-app"
-  location            = azurerm_resource_group.self_service_rg.location
-  resource_group_name = azurerm_resource_group.self_service_rg.name
-  app_service_plan_id = azurerm_app_service_plan.service_plan.id
+resource "azurerm_app_service" "app" {
+  name                = "app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+  site_config {
+    always_on = true
+  }
 }
 
-resource "azurerm_storage_account" "storage_account" {
-  name                     = "selfservicestorage"
-  resource_group_name      = azurerm_resource_group.self_service_rg.name
-  location                 = azurerm_resource_group.self_service_rg.location
+resource "azurerm_key_vault" "example" {
+  name                        = "examplevault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  soft_delete_enabled         = true
+  sku_name                    = "standard"
+}
+
+resource "azurerm_api_management" "example" {
+  name                = "example-api"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Developer"
+  publisher_name      = "pub1"
+  publisher_email     = "pub1@contoso.com"
+}
+
+resource "azurerm_servicebus_namespace" "example" {
+  name                = "example-sb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "mysqlserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "adminlogin"
+  administrator_login_password = "H@Sh1CoR3!"
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.example.name
+  edition             = "Basic"
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  consistency_policy {
+    consistency_level = "Session"
+  }
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "storage_container" {
-  name                  = "selfservicecontainer"
-  storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = "private"
+resource "azurerm_monitor" "example" {
+  name                = "example-monitor"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_sql_server" "sql_server" {
-  name                         = "selfservicesqlserver"
-  resource_group_name          = azurerm_resource_group.self_service_rg.name
-  location                     = azurerm_resource_group.self_service_rg.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
+resource "azurerm_security_center_contact" "example" {
+  email   = "email@contoso.com"
+  phone   = "123456789"
+  name    = "default"
 }
 
-resource "azurerm_sql_database" "sql_database" {
-  name                = "selfservicedb"
-  resource_group_name = azurerm_resource_group.self_service_rg.name
-  location            = azurerm_resource_group.self_service_rg.location
-  server_name         = azurerm_sql_server.sql_server.name
-  requested_service_objective_name = "S1"
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "example-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
 }
 
-resource "azurerm_logic_app" "logic_app" {
-  name                = "selfservicelogicapp"
-  resource_group_name = azurerm_resource_group.self_service_rg.name
-  location            = azurerm_resource_group.self_service_rg.location
-}
-
-resource "azurerm_servicebus_namespace" "servicebus_namespace" {
-  name                = "selfserviceservicebus"
-  resource_group_name = azurerm_resource_group.self_service_rg.name
-  location            = azurerm_resource_group.self_service_rg.location
-  sku                 = "Standard"
-}
-
-resource "azurerm_function_app" "function_app" {
-  name                = "selfservicefunctionapp"
-  location            = azurerm_resource_group.self_service_rg.location
-  resource_group_name = azurerm_resource_group.self_service_rg.name
-  app_service_plan_id = azurerm_app_service_plan.service_plan.id
-  storage_account_name = azurerm_storage_account.storage_account.name
-  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
-  version = "~3"
-}
-
-resource "azurerm_application_insights" "app_insights" {
-  name                = "selfserviceappinsights"
-  location            = azurerm_resource_group.self_service_rg.location
-  resource_group_name = azurerm_resource_group.self_service_rg.name
+resource "azurerm_application_insights" "example" {
+  name                = "example-ai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
 }
 
-resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
-  name                        = "selfservicediagnosticsetting"
-  target_resource_id          = azurerm_app_service.app_service.id
-  log_analytics_workspace_id  = azurerm_application_insights.app_insights.id
-  enabled_log {
-    category = "AppServiceConsoleLogs"
-    enabled  = true
-  }
-  metric {
-    category = "AllMetrics"
-    enabled  = true
-  }
+resource "azurerm_sentinel" "example" {
+  monitoring_workspace_id = azurerm_log_analytics_workspace.example.id
 }
