5adecbc33db35671e8377a50f2f0382a38739579
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 03:03:34 +0530

    commited by n8n

--- backend/dashboard/repositories/transaction_repository.py
+++ backend/dashboard/repositories/transaction_repository.py
@@ -5,13 +5,8 @@ from backend.app import db
 
 class TransactionRepository:
     @staticmethod
-    def save(transaction: Transaction) -> None:
-        db.session.add(transaction)
-        db.session.commit()
-
-    @staticmethod
     def find_by_account_id(account_id: int) -> list[Transaction]:
         return Transaction.query.filter_by(account_id=account_id).all()
 
 
-# File 6: Dashboard Service to Fetch User Account Data in dashboard/services/dashboard_service.py
+# File 4: Dashboard Controller to Display Personalized Dashboard in dashboard/controllers/dashboard_controller.py
