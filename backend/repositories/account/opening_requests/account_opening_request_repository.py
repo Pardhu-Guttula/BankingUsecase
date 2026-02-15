fdd34a2868538d392262b73c4a7fed5626e37ca1
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 04:31:08 +0530

    commited by n8n

--- backend/repositories/account/opening_requests/account_opening_request_repository.py
+++ backend/repositories/account/opening_requests/account_opening_request_repository.py
@@ -1,4 +1,4 @@
-# Epic Title: Account Opening and Service Modifications
+# Epic Title: Streamline Account Opening Requests
 
 from backend.models.account.opening_requests.account_opening_request_model import AccountOpeningRequest
 from backend.app import db
@@ -10,11 +10,8 @@ class AccountOpeningRequestRepository:
         db.session.commit()
 
     @staticmethod
-    def get_by_user_id(user_id: int) -> list[AccountOpeningRequest]:
+    def find_by_user_id(user_id: int) -> list[AccountOpeningRequest]:
         return AccountOpeningRequest.query.filter_by(user_id=user_id).all()
 
-    @staticmethod
-    def get_by_id(request_id: int) -> AccountOpeningRequest:
-        return AccountOpeningRequest.query.filter_by(id=request_id).first()
 
 # File 3: Account Opening Request Service in services/account/opening_requests/account_opening_request_service.py
