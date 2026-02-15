aefe9b6e439d9254fb9ee57527209e611d632eea
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 02:05:16 +0530

    commited by n8n

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
-# Epic Title: Develop Secure APIs
+# Epic Title: Save and Resume Incomplete Applications
 
 from flask import Flask
 from authentication.controllers.auth_controller import auth_controller
 from interaction_history.controllers.application_controller import application_controller
 from interaction_history.controllers.document_controller import document_controller
 from interaction_history.controllers.interaction_controller import interaction_controller
-from core_integration.helpers import register_blueprints, configure_cors
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask_login import LoginManager
@@ -17,9 +16,6 @@ app.register_blueprint(auth_controller, url_prefix='/auth')
 app.register_blueprint(application_controller, url_prefix='/api')
 app.register_blueprint(document_controller, url_prefix='/api')
 app.register_blueprint(interaction_controller, url_prefix='/api')
-
-register_blueprints(app)
-configure_cors(app)
 
 DATABASE_URI = 'mysql+pymysql://username:password@localhost/db_name'
 engine = create_engine(DATABASE_URI)
@@ -48,4 +44,4 @@ if __name__ == '__main__':
     socketio.run(app, debug=True)
 
 
-# File 5: requirements.txt Update
+# File 7: Schema Definition for Application Table in database/03_create_applications_table.sql
