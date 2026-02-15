f5cf269657f46483d484360760659b3a0905009e
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 04:01:03 +0530

    commited by n8n

--- backend/repositories/account/modifications/service_modification_repository.py
+++ backend/repositories/account/modifications/service_modification_repository.py
@@ -1,21 +1,27 @@
 # Epic Title: Account Opening and Service Modifications
 
-from backend.models.account.modifications.service_modification_model import ServiceModificationRequest
+from backend.models.account.modifications.service_modification_model import ServiceModification
 from backend.app import db
 
-class ServiceModificationRequestRepository:
+class ServiceModificationRepository:
     @staticmethod
-    def save(service_modification_request: ServiceModificationRequest) -> None:
-        db.session.add(service_modification_request)
+    def save(service_modification: ServiceModification) -> None:
+        db.session.add(service_modification)
         db.session.commit()
 
     @staticmethod
-    def get_requests_by_user(user_id: int) -> list[ServiceModificationRequest]:
-        return ServiceModificationRequest.query.filter_by(user_id=user_id).all()
+    def get_by_user_id(user_id: int) -> list[ServiceModification]:
+        return ServiceModification.query.filter_by(user_id=user_id).all()
 
     @staticmethod
-    def get_request_by_id(request_id: int) -> ServiceModificationRequest:
-        return ServiceModificationRequest.query.get(request_id)
+    def get_by_id(service_modification_id: int) -> ServiceModification:
+        return ServiceModification.query.filter_by(id=service_modification_id).first()
 
+    @staticmethod
+    def update_status(service_modification_id: int, status: str) -> None:
+        service_modification = ServiceModificationRepository.get_by_id(service_modification_id)
+        if service_modification:
+            service_modification.status = status
+            db.session.commit()
 
-# File 3: Service Modification Request Service in services/account/modifications/service_modification_service.py
+# File 3: Service Modification Service in services/account/modifications/service_modification_service.py
