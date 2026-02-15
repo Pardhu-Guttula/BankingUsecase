b4dc763218dd042ccb6d44bf7f731732d19645c6
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 04:39:53 +0530

    commited by n8n

--- backend/services/portal_main_database/portal_main_service.py
+++ backend/services/portal_main_database/portal_main_service.py
@@ -1,17 +1,21 @@
-# Epic Title: Core Banking System Integration
+# Epic Title: Maintain Separate Database
 
-from backend.models.portal_main_database.portal_main_model import PortalMainModel
 from backend.repositories.portal_main_database.portal_main_repository import PortalMainRepository
+from backend.models.portal_main_database.portal_main_model import PortalMainData
 
 class PortalMainService:
     @staticmethod
-    def add_data(data: str) -> PortalMainModel:
-        data_entry = PortalMainModel(data=data)
-        PortalMainRepository.save(data_entry)
-        return data_entry
+    def save_data(data_key: str, data_value: str) -> None:
+        data = PortalMainData(data_key=data_key, data_value=data_value)
+        PortalMainRepository.save(data)
 
     @staticmethod
-    def get_all_data() -> list[PortalMainModel]:
-        return PortalMainRepository.get_all()
+    def get_data_by_key(data_key: str) -> PortalMainData:
+        return PortalMainRepository.find_by_key(data_key)
 
-# File 4: Portal Database Controller in controllers/portal_main_database/portal_main_controller.py
+    @staticmethod
+    def get_all_data() -> list[PortalMainData]:
+        return PortalMainRepository.find_all()
+
+
+# File 4: Portal Main Database Controller in `controllers/portal_main_database/portal_main_controller.py`
