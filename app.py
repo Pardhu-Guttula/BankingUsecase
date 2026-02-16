0ea486c13ce5ba15664f15b0e90339a05a2cf103
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 12:04:35 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-# Epic Title: Manage Secure Storage of Credentials
+# Epic Title: Overview of Financial Activities
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from backend.authentication.routes.auth_routes import auth_routes
+from backend.dashboard.routes.dashboard_routes import register_dashboard_routes
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@localhost/mydatabase'
+app.config['JWT_SECRET_KEY'] = 'super-secret' 
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
 app.register_blueprint(auth_routes, url_prefix='/auth')
+register_dashboard_routes(app)
 
 if __name__ == '__main__':
     import logging
