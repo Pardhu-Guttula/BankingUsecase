d8920617b4ada875c089506fbaa7b4ab5c931007
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 12:12:57 +0530

    commited by n8n

--- backend/integration/routes.py
+++ backend/integration/routes.py
@@ -2,8 +2,7 @@
 
 from backend.integration.controllers import sync_blueprint
 
-def register_sync_routes(app, data_sync_service_instance, db_instance):
-    global data_sync_service, db
+def register_sync_routes(app, data_sync_service_instance):
+    global data_sync_service
     data_sync_service = data_sync_service_instance
-    db = db_instance
     app.register_blueprint(sync_blueprint, url_prefix='/api')
