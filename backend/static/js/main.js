b99ca01411006b80afa66725b037148603fd2892
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-16 13:18:49 +0530

    commited by n8n

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -4,7 +4,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('JavaScript loaded successfully!');
     var socket = io.connect('http://' + document.domain + ':' + location.port);
-
     socket.on('status_update', function(msg) {
         var statusElement = document.getElementById("status-" + msg.request_id);
         if (statusElement) {
@@ -12,19 +11,5 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             console.log("Status element for request " + msg.request_id + " not found.");
         }
-        showNotification(msg.status, msg.request_id);
     });
-
-    function showNotification(message, requestId) {
-        const notificationsDiv = document.getElementById("notifications");
-        const notification = document.createElement("div");
-        notification.classList.add("notifications");
-        notification.textContent = `Request ${requestId}: ${message}`;
-
-        notificationsDiv.appendChild(notification);
-
-        setTimeout(() => {
-            notification.remove();
-        }, 5000);
-    }
 });
